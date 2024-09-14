3127cb68c059b9ff8d6a41438f4105c9c153bb7b
金昊聪 <jinhaocong@outlook.com>
2024-09-14 15:14:09 +0800

    feat: 添加代理服务器

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,16 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: '/GeoLite2/',
   plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://download.maxmind.com', // 目标服务器
+        changeOrigin: true, // 确保代理请求时，使用目标服务器的Origin
+        secure: false, // 忽略https的SSL验证（如果必要）
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
